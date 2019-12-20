06d0fbba4df3c0dc1b8fce1e8cb17fde0b72ce3f
â13019880466 <457991194@qq.com>
2019-12-20 18:42:37 +0800

    Updates

---
+++ precache-manifest.80016de20f2b34190f4d8714d6eec2cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63b6a3c61ed97b4b284359e65b819e78",
+    "url": "/aliTest/index.html"
+  },
+  {
+    "revision": "2504e349f92d4f907f92",
+    "url": "/aliTest/static/css/main.2942db99.chunk.css"
+  },
+  {
+    "revision": "a7a7b85d17e0daa69d0d",
+    "url": "/aliTest/static/js/2.8eb75daf.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/aliTest/static/js/2.8eb75daf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2504e349f92d4f907f92",
+    "url": "/aliTest/static/js/main.69df2414.chunk.js"
+  },
+  {
+    "revision": "ab688d065f96914e300b",
+    "url": "/aliTest/static/js/runtime-main.ceb817e8.js"
+  }
+]);
